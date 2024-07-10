bb39f26e9ba65e7ec3eec39d7a3e075b0fd8b29b
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2024-07-10 11:43:49 +0200

    refactor(web-app): remove project
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- web-app/src/constants/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { enGB as dateFnsEN, fr as dateFnsFR } from "date-fns/locale";
-
-export const fallbackLng = "en" as const;
-export const languages = [fallbackLng, "fr"] as const;
-
-export const dateFnsLocales = {
-  [languages[0]]: dateFnsEN,
-  [languages[1]]: dateFnsFR,
-} as const;
--- web-app/src/types/skill.ts
+++
@@ -1,6 +0,0 @@
-import { Technology } from "./technology";
-
-export type Skill = {
-  technology: Technology;
-  years: number;
-};
